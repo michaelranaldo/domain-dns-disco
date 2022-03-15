e638502bed259894cef1631f80cf7f1979b19914
Michael Ranaldo <michael@ranaldo.co.uk>
2022-03-15 15:50:33 +0000

    Added output

--- domain-dns-disco.py
+++ domain-dns-disco.py
@@ -1,14 +1,19 @@
 #!python3
 
-import dns.resolver, argparse
+import dns.resolver, argparse, os.path
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("domain", help="The domain to request")
+parser.add_argument("outfile", help="The output file location")
 args = parser.parse_args()
 
-
+if os.path.exists(args.outfile):
+    print("Output file already exists")
+    print("Quitting...")
+    quit()
 
 dns_records = ["_ldap._tcp", "_gc._tcp", "_kerberos._tcp", "_kpasswd._tcp"]
+hosts = []
 
 for dns_record in dns_records:
     print()
@@ -18,8 +23,19 @@ for dns_record in dns_records:
         dns_response = dns.resolver.query(dns_record + "." + args.domain, "SRV").response
         if len(dns_response.answer) > 0:
             for host in dns_response.answer[0]:
-                print(host.target)
+                neathost = str(host.target).rstrip(".")
+                print(neathost.lower())
+                hosts.append(neathost.lower())
         else:
             raise Exception
     except:
         print("No results for " + dns_record + "." + args.domain)
+
+    f = open(args.outfile, "a")
+
+    unique_hosts = []
+    for host in hosts:
+        if host not in unique_hosts:
+            f.write(host + "\n")
+            unique_hosts.append(host)
+    f.close()
